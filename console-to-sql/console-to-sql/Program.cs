a72fcdfbafcb0fe8ddacfdb305b008592c6b8e6d
sunny.gulati <sunnywiz@gmail.com>
2016-10-01 21:04:36 -0700

    Initial version, C# talking to SQL, via SQLCommand, talking to external database already defined

---
+++ console-to-sql/console-to-sql/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_to_sql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection("data source=(localdb)\\mssqllocaldb; database=StarWars"))
+            {
+                connection.Open();
+                using (var cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = "select PersonId, Name, HeightInMeters from Person";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var personID = reader.GetInt32(0);
+                            var name = reader.GetString(1);
+                            decimal? height;
+                            if (reader.IsDBNull(2)) height = null; else  height = reader.GetDecimal(2);
+
+                            Console.WriteLine("{0}: {1} {2}m", personID, name, height);
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
